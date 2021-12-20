3a115b86cc105fca9285cd0f9d3242162f596969
amozalev <mozalev.alexey@gmail.com>
2021-12-20 17:46:36 +0300

    Set up Jest tests.

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,10 @@
+import './__mocks__/matchMediaMock';
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    render(<App />);
+    const linkElement = screen.getByText(/MQTT Connection/i);
+    expect(linkElement).toBeInTheDocument();
 });
